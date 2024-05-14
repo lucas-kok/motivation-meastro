2858d8025a60038b65db5be4b0230e53660c2949
Matthijs Feringa <mt.feringa@student.avans.nl>
2024-05-14 13:06:10 +0200

    first part of final room scene

--- Serious Game/Assets/TempFinalSceneManager.cs
+++ Serious Game/Assets/TempFinalSceneManager.cs
@@ -1,18 +1,35 @@
 
+using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class TempFinalSceneManager : MonoBehaviour
 {
-    private GameState _gameState; 
+    private GameState _gameState;
+    private List<Scenario> scenarios;
 
-    public TextMeshProUGUI statsText;
+    public GameObject content;
+    public GameObject template;
 
     void Start()
     {
         _gameState = GameState.Instance;
-        
-        statsText.SetText(_gameState.ToString());   
+        //scenarios = _gameState.Scenarios;
+        scenarios = new List<Scenario>() { new Scenario(), new Scenario(), new Scenario(), new Scenario(), new Scenario() };
+
+        foreach(var scenario in scenarios)
+        {
+            GameObject duplicatedObject = Instantiate(template, new Vector3(0, 0, 0), Quaternion.identity);
+
+            var tmp = duplicatedObject.GetComponentInChildren<TextMeshProUGUI>();
+            tmp.text = scenario.Title;
+
+            // Set the parent of the duplicated object
+            duplicatedObject.transform.SetParent(content.transform, false);
+
+
+        }
     }
 
 }
